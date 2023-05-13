130930fda0fd9bfaa46956390127ef8eade82cfa
ebaltierra <ezzy123we@gmail.com>
2023-05-13 02:29:23 -0500

    add variabvle libaray

--- DINO_GAME/GAME FINAL PROJECT.py
+++ DINO_GAME/GAME FINAL PROJECT.py
@@ -1,2 +1,27 @@
-stats=open("Minion_stats.py", "r")
-print(stats.read())
+from Minion_stats import *
+from Minion_Emblem import minion1
+from Minion_Emblem import Gru_emblem
+from Minion_Emblem import Lucy_emblem
+def lb(): 
+    print("\n========================================================================")
+    input()
+    
+lb()
+minion1()
+print("\nHELLO, WELCOME TO MINLANDIA SIMULATION CREATED BY MARGO, AGNES, AND EDITH ")
+lb()
+print("\nWho is playing in this simulation? (gru/lucy)")
+lb()
+while True:
+    character=input("\n: ")
+    if character==str("gru"):
+        Gru_emblem
+        print("\nGreat, lets continue with our simulation")
+        break
+    elif character==str("lucy"):
+        Lucy_emblem
+        print("\nGreat, lets continue with our simulation")
+        break
+    else:
+        print("\nThat's not a character silly, try again.")
+
